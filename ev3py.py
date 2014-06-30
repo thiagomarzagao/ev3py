977b10d718fbd664f776eee8d97779f6be282bf1
Thiago Marzagão <tmarzagao@gmail.com>
2014-06-30 15:25:55 -0400

    added opOUTPUT_STEP_POWER; streamlined docstrings

--- ev3py.py
+++ ev3py.py
@@ -27,20 +27,23 @@ PRIMPAR_SHORT = 0x00
 PRIMPAR_LONG = 0x80
 PRIMPAR_CONST = 0x00
 PRIMPAR_VALUE = 0x3F
+PRIMPAR_GLOBAL = 0x20
+PRIMPAR_INDEX = 0x1F
+PRIMPAR_VARIABEL = 0x40
 PRIMPAR_1_BYTE = 1
 PRIMPAR_2_BYTES = 2
-PRIMPAR_4_BYTES = 4
+PRIMPAR_4_BYTES = 3
 
 def LC0(v):
     '''
-    format 1-byte argument
+    format 1-byte local constant
     '''
     byte1 = ((v & PRIMPAR_VALUE) | PRIMPAR_SHORT | PRIMPAR_CONST)
     return h[byte1]
 
 def LC1(v):
     '''
-    format 2-byte argument
+    format 2-byte local constant
     '''    
     byte1 = (PRIMPAR_LONG | PRIMPAR_CONST | PRIMPAR_1_BYTE)
     byte2 = (v & 0xFF)
@@ -48,17 +51,50 @@ def LC1(v):
 
 def LC2(v):
     '''
-    format 3-byte argument
+    format 3-byte local constant
     '''    
     byte1 = (PRIMPAR_LONG | PRIMPAR_CONST | PRIMPAR_2_BYTES)
     byte2 = (v & 0xFF)
     byte3 = ((v >> 8) & 0xFF)
     return h[byte1] + h[byte2] + h[byte3]
 
+def LC4(v):
+    '''
+    format 5-byte local constant
+    '''
+    byte1 = (PRIMPAR_LONG  | PRIMPAR_CONST | PRIMPAR_4_BYTES)
+    byte2 = (v & 0xFF)
+    byte3 = ((v >> 8) & 0xFF)
+    byte4 = ((v >> 16) & 0xFF)
+    byte5 = ((v >> 24) & 0xFF)
+    return h[byte1] + h[byte2] + h[byte3] + h[byte4] + h[byte5]
+
+def GV0(v):
+    '''
+    format 1-byte global variable
+    '''
+    return h[((v & PRIMPAR_INDEX) | PRIMPAR_SHORT | PRIMPAR_VARIABEL | PRIMPAR_GLOBAL)]
+
 class ev3:
 
     '''
-    this class lets you interact with EV3 bricks    
+    this class lets you interact with EV3 bricks
+    
+    arguments used in several methods:
+    
+    ports: a, b, c, d, any combinations thereof
+        type: str or iterable
+        obs.: motor ports
+        examples: 'a', 'bcd', ['d', 'c'], ['a'], ('c', 'b')        
+    port: 0, 1, 2, 3
+        type: int
+        obs.: sensor port
+    power: -100...+100
+        type: int
+        obs.: -100 is full power backward, 100 is full power forward
+    layer: 0, 1, 2, 3
+        type: int
+        obs.: use 0 unless you have multiple EV3 bricks
     '''
 
     def __init__(self):
@@ -69,32 +105,23 @@ class ev3:
 
         self.ports_to_int = {'a': 1, 'b': 2, 'c': 4, 'd': 8}
 
-    def connect(self, mode):
+    def connect(self, conn_type):
     
         '''
         connect to EV3
 
-        mode: 'bt' (bluetooth), 'usb', 'wifi'
+        conn: 'bt' (bluetooth), 'usb', 'wifi'
         '''
 
-        modes = {'bt': '/dev/tty.EV3-SerialPort',
-                 'usb': '', # must add usb support
-                 'wifi': ''} # must add wifi support
-        self.brick = open(modes[mode], mode = 'w+', buffering = 0)
+        conn_types = {'bt': '/dev/tty.EV3-SerialPort',
+                      'usb': '', # must add usb support
+                      'wifi': ''} # must add wifi support
+        self.brick = open(conn_types[conn_type], mode = 'w+', buffering = 0)
 
     def start_motors(self, ports, power, layer=0):
 
         '''
-        start motors at specified ports, power, and layer
-        
-        ports: a, b, c, d, any combinations thereof
-            type: str or iterable
-            examples: 'a', 'bcd', ['d', 'c'], ['a'], ('c', 'b')
-        power: -100...+100
-            type: int
-            obs.: -100 is full power backward, 100 is full power forward
-        layer: 0, 1, 2, 3 (use 0 unless you have multiple EV3 bricks)
-            type: int
+        start motors at specified ports, power, and layer        
         '''
 
         # map ports: str->int
@@ -113,26 +140,21 @@ class ev3:
         # assemble command and send to EV3
         command = comm_0 + comm_1 + comm_2
         self.brick.write(command)
-
+                
     def stop_motors(self, ports, mode='coast', layer=0):
  
         '''
         stop motors at specified ports and layer
-        
-        ports: a, b, c, d, any combinations thereof
-            type: str or iterable
-            examples: 'a', 'bcd', ['d', 'c'], ['a'], ('c', 'b')
-        mode: 'break', 'coast'
+
+        mode: 'brake', 'coast'
             type: str
-        layer: 0, 1, 2, 3 (use 0 unless you have multiple EV3 bricks)
-            type: int
         '''
 
         # map ports: str->int
         ports = sum([self.ports_to_int[port] for port in ports])
 
         # map mode: str->int
-        modes = {'break': 1, 'coast': 0}
+        modes = {'brake': 1, 'coast': 0}
         mode = modes[mode]
  
         # set message size, message counter, command type, vars
@@ -145,13 +167,53 @@ class ev3:
         command = comm_0 + comm_1
         self.brick.write(command)
 
+    def start_motors_degrees(self, ports, power, degrees, mode='brake', 
+                             ramp_up=0, ramp_down=0, layer=0):
+
+        '''
+        start motors at specified ports, power, and layer, and
+        stop them after specified number of degrees (accurate to
+        +/- 1 degree)
+        
+        degrees: 0...MAX
+            type: int
+            obs.: unit is degrees (360 degrees = 1 full turn)
+        mode: 'brake', 'coast'
+            type: str
+        ramp_up: 0...MAX
+            type: int
+            obs.: makes acceleration constant; unit is degrees
+        ramp_down: 0...MAX
+            type: int
+            obs.: makes deceleration constant; unit is degrees
+        '''        
+
+        # map ports: str->int
+        ports = sum([self.ports_to_int[port] for port in ports])
+
+        # map mode: str->int
+        modes = {'brake': 1, 'coast': 0}
+        mode = modes[mode]
+
+        # opOUTPUT_STEP_POWER
+        comm_1 = '\xAC' + LC0(layer) + LC0(ports) + LC1(power) \
+                 + LC4(ramp_up) + LC4(degrees) + LC4(ramp_down) \
+                 + LC0(mode)
+    
+        # opOUTPUT_START
+        comm_2 = '\xA6' + LC0(layer) + LC0(ports)
+
+        msg_size = len(comm_1 + comm_2) + 5
+        comm_0 = h[msg_size] + '\x00\x00\x00\x80\x00\x00'
+
+        # assemble command and send to EV3
+        command = comm_0 + comm_1 + comm_2
+        self.brick.write(command)
+
     def read_sensor(self, port, layer=0):
         
         '''
         read sensor from specified port (unit: percentage)
-        
-        port: 0, 1, 2, 3
-        layer: 0, 1, 2, 3 (use 0 unless you have multiple EV3 bricks)
         '''
 
         # set message size, message counter, command type, vars
@@ -167,8 +229,6 @@ class ev3:
         # retrieve sensor value (5th byte) and convert to int
         sensor_data = self.brick.read(6)
         return int(hex(ord(sensor_data[5])), 16)
-
-    def get_output_type
 
     def disconnect(self):
         self.brick.close()
